a4778a0da7a9403befc6c2a569d6afd6a28cfa53
jatosdigitalstudio <jatosajibuana@gmail.com>
2025-07-13 22:57:57 +0700

    ver 1.3

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -4,20 +4,18 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative h-[40rem] overflow-hidden mx-6 md:mx-20 bg-blue-900 flex items-center rounded-xl">
+    <section className="relative h-full md:h-[40rem] overflow-hidden mx-6 md:mx-20 bg-blue-900 flex items-center rounded-xl">
         <div className="container mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
-                <div className="md:pl-18 text-white">
-                    <div className="">
-                        <h1 className="text-2xl leading-[1.4] font-style md:text-4xl mb-4 font-bold">
-                            Selamat Datang di Website <br/>
-                            Koperasi Desa Merah Putih Sijunjung
-                        </h1>
-                        <p className="text-md md:text-lg mb-4">
-                            Bersama, kita membangun ekonomi desa yang mandiri dan sejahtera melalui koperasi berbasis 
-                            gotong royong dan digitalisasi.
-                        </p>
-                    </div>
+                <div className="pt-12 px-6 md:pl-18 text-white">
+                    <h1 className="text-2xl leading-[1.4] font-style md:text-4xl mb-4 font-bold">
+                        Selamat Datang di Website <br/>
+                        Koperasi Desa Merah Putih Sijunjung
+                    </h1>
+                    <p className="text-md md:text-lg mb-4">
+                        Bersama, kita membangun ekonomi desa yang mandiri dan sejahtera melalui koperasi berbasis 
+                        gotong royong dan digitalisasi.
+                    </p>
                     <div className="flex flex-col md:flex-row mx-auto mt-8 justify-start gap-2 md:gap-4">
                         <Link href="/#about" scroll className="border border-white px-6 py-3 rounded-md">
                             Lihat Profil Koperasi
